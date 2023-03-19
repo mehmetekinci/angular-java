c1badeed333df5f46ee71a233743dbe15a22a87d
mehmetekinci <mehmetekinci1989@gmail.com>
2023-03-19 18:29:14 +0100

    added bootstrap library and implemented added components

--- frontend-todo/src/app/app-routing.module.ts
+++ frontend-todo/src/app/app-routing.module.ts
@@ -1,23 +1,41 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
-import { LoginComponent } from './login/login.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ErrorComponent } from './error/error.component';
-import { ListTodosComponent } from './list-todos/list-todos.component';
+import { LoginComponent } from "./login/login.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { ErrorComponent } from "./error/error.component";
+import { ListTodosComponent } from "./list-todos/list-todos.component";
 
+import { RouteGuardService } from "./services/routeGuard/route-guard.service";
 
 const routes: Routes = [
-  {path: "", component: LoginComponent},
-  {path: "login", component: LoginComponent},
-  {path: "welcome/:name", component: WelcomeComponent},
-  {path: "todos", component: ListTodosComponent},
-  {path: "**", component: ErrorComponent},
+  { path: "", component: LoginComponent },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "logout",
+    component: LogoutComponent,
+    canActivate: [RouteGuardService],
+  },
+  {
+    path: "welcome/:name",
+    component: WelcomeComponent,
+    canActivate: [RouteGuardService],
+  },
+  {
+    path: "todos",
+    component: ListTodosComponent,
+    canActivate: [RouteGuardService],
+  },
+  { path: "**", component: ErrorComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), FormsModule],
-exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
