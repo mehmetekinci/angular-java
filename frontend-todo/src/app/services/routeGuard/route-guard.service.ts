14f10df53952c9bb5cbdcd4d5b8e48f50832ab33
mehmetekinci <mehmetekinci1989@gmail.com>
2023-03-19 18:28:27 +0100

    created route-gurad service to protect routes

---
+++ frontend-todo/src/app/services/routeGuard/route-guard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  Router,
+} from "@angular/router";
+import { AuthService } from "../auth/auth.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class RouteGuardService implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isUserLoggedIn()) {
+      return true;
+    }
+    this.router.navigate(["login"]);
+    return false;
+  }
+}
