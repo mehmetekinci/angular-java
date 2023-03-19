36068aafc6fc315e9f6a459afd4bb430a1cfc9d7
mehmetekinci <mehmetekinci1989@gmail.com>
2023-03-19 18:26:08 +0100

    modified liast-todo component

--- frontend-todo/src/app/list-todos/list-todos.component.ts
+++ frontend-todo/src/app/list-todos/list-todos.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {}
+}
 
 @Component({
-  selector: 'app-list-todos',
-  templateUrl: './list-todos.component.html',
-  styleUrls: ['./list-todos.component.scss']
+  selector: "app-list-todos",
+  templateUrl: "./list-todos.component.html",
+  styleUrls: ["./list-todos.component.scss"],
 })
 export class ListTodosComponent implements OnInit {
-
   todos = [
-    {id: 1, description: 'learn dutch.'},
-    {id: 2, description: 'learn Python.'},
-    {id: 3, description: 'learn English.'},
-    {id: 4, description: 'learn Back-end coding.'},
-    {id: 5, description: 'start to fix issues from ARSpar.'},
-  ]
-  constructor() { }
+    new Todo(1, "learn dutch.", false, new Date()),
+    new Todo(2, "learn Python.", false, new Date()),
+    new Todo(3, "learn English.", false, new Date()),
+    new Todo(4, "learn Back-end Development", false, new Date()),
+    new Todo(5, "start to fix issues from ARSpar.", false, new Date()),
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
