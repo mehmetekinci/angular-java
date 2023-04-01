262a023ff72d39687dcaca3b42bdfda309bdd00c
mehmetekinci <mehmetekinci1989@gmail.com>
2023-04-01 09:15:17 +0200

    added delete and update todo buttons and functions

--- frontend-todo/src/app/list-todos/list-todos.component.ts
+++ frontend-todo/src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { TodoDataService } from "../services/data/todo-data.service";
+import { Router } from "@angular/router";
 
 export class Todo {
   constructor(
@@ -15,14 +17,36 @@ export class Todo {
   styleUrls: ["./list-todos.component.scss"],
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
-    new Todo(1, "learn dutch.", false, new Date()),
-    new Todo(2, "learn Python.", false, new Date()),
-    new Todo(3, "learn English.", false, new Date()),
-    new Todo(4, "learn Back-end Development", false, new Date()),
-    new Todo(5, "start to fix issues from ARSpar.", false, new Date()),
-  ];
-  constructor() {}
+  // todos = [
+  //   new Todo(1, "learn dutch.", false, new Date()),
+  //   new Todo(2, "learn Python.", false, new Date()),
+  //   new Todo(3, "learn English.", false, new Date()),
+  //   new Todo(4, "learn Back-end Development", false, new Date()),
+  //   new Todo(5, "start to fix issues from ARSpar.", false, new Date()),
+  // ];
+  todos: Todo[];
+  message: string;
+  constructor(private todoService: TodoDataService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateTodos();
+  }
+
+  updateTodos() {
+    this.todoService.retrieveAllTodos("memo").subscribe((response) => {
+      this.todos = response;
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todoService.deleteTodo("memo", id).subscribe((response) => {
+      console.log("response", response);
+      this.message = `Delete of todo ${id} successful!`;
+      this.updateTodos();
+    });
+  }
+  updateTodo(id: number) {
+    this.message = `Update of todo ${id} successful!`;
+    this.router.navigate(["todos", id]);
+  }
 }
